b9368ba2c510680bd491ab9d086dd02d5f11ebe7
Jake Delaney <jakedelaney24@gmail.com>
2023-11-27 21:24:13 +0000

    Update with solved problems

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,23 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum1 = sumAll(1, 5, 2);
+console.log("Sum 1:", sum1);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 5, 2, 2, 5);
+console.log("Sum 2:", sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest){
+        sum += i;
+    }
+
+    return sum;
+}
+
+let sum3 = sumRest(1, 5, 2, 2, 5)
+console.log("Sum 3:", sum3)
